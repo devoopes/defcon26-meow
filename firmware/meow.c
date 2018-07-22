73203363ef8ce0b4ecd7a7fd1fc7bc1db23609de
dsamarin1 <dsamarin1@gmail.com>
2018-07-21 17:48:02 -0700

    blank new project for stm32f030_8 (we are using c8t6)

---
+++ firmware/meow.c
@@ -0,0 +1,13 @@
+/*************************************************
+	Maneki Neko (Lucky Cat) badge
+	DEF CON 26, 2018
+	#badgelife
+	Authors: tense future & dash
+	https://github.com/SeanLeftBelow/defcon26-meow
+*************************************************/
+
+
+int main()
+{
+	// do stuff
+}
